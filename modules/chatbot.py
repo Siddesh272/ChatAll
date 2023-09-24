006a4644554a1827e3d1777aee81b5de2eab2ed7
Siddesh Mishra <90123105+Siddesh272@users.noreply.github.com>
2023-09-24 13:45:55 +0530

    Update chatbot.py

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 from transformers import pipeline
 from sentence_transformers import SentenceTransformer, util
 
-chatbot = pipeline("conversational")
+chatbot = pipeline("conversational",model="gpt-2.5-turbo")
 
 faq_responses = {}
 with open("static/faq_responses.txt", "r") as faq_file:
