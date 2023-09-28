ed6807c3904e4d09c5b9f993c8bfa84323708f17
Siddesh272 <90123105+Siddesh272@users.noreply.github.com>
2023-09-28 13:10:41 +0530

    styled

--- modules/chatbot.py
+++ modules/chatbot.py
@@ -4,7 +4,7 @@ import speech_recognition as sr
 from transformers import pipeline
 from sentence_transformers import SentenceTransformer, util
 
-chatbot = pipeline("conversational",model="distilbert-base-uncased")
+chatbot = pipeline("conversational")
 
 faq_responses = {}
 with open("static/faq_responses.txt", "r") as faq_file:
@@ -13,32 +13,20 @@ with open("static/faq_responses.txt", "r") as faq_file:
         q, a = line.strip().split("#")
         faq_responses[q] = a
 
-sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2',embedding_dim=128)
+sentence_model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 def text_chatbot(user_input):
     response = pipeline("conversational")(user_input)[0]['generated_text']
     return response
 
-import faiss
-
 def semantic_search(query, faq_responses, threshold=0.7):
-  query_embedding = sentence_model.encode([query], convert_to_tensor=True)
-
-  # Create a Faiss index
-  index = faiss.IndexFlatL2(128)
-
-  # Add the FAQ embeddings to the index
-  index.add(sentence_model.encode(list(faq_responses.keys())))
-
-  # Search for the most similar FAQ questions
-  distances, indices = index.search(query_embedding, k=10)
-
-  similar_questions = []
-  for i in indices[0]:
-    if distances[0][i] < threshold:
-      similar_questions.append(list(faq_responses.keys())[i])  
-
-  return similar_questions
-
+    query_embedding = SentenceTransformer('paraphrase-MiniLM-L6-v2').encode([query], convert_to_tensor=True)
+    faq_embeddings = SentenceTransformer('paraphrase-MiniLM-L6-v2').encode(list(faq_responses.keys()), convert_to_tensor=True)
+    cos_scores = util.pytorch_cos_sim(query_embedding, faq_embeddings)[0]
+    similar_questions = []
+    for i, score in enumerate(cos_scores):
+        if score > threshold:
+            similar_questions.append(list(faq_responses.keys())[i])  
+    return similar_questions
 
 def get_response(user_input):
     user_input = user_input.strip().lower()
